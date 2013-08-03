90160105d2253ec0b42bd3da8ed80b0953c83c42
Steven Luo <steven@steven676.net>
2013-08-02 23:59:57 -0700

    Revert "usb: musb: fix pm_runtime mismatch"
    Bisection identified this commit as causing core_pwrdm to not got to
    sleep when the system suspends.  Presumably some change in the omapzoom
    tree or one of our local changes adds a pm_runtime_get(), causing the
    USB controller to never sleep when this patch is applied.
    
    This reverts commit a2cb6c3022494f6e43d284ed9159e15dd71f5937.

--- drivers/usb/musb/musb_core.c
+++ drivers/usb/musb/musb_core.c
@@ -2160,6 +2160,7 @@ musb_init_controller(struct device *dev, int nIrq, void __iomem *ctrl)
 	if (is_otg_enabled(musb) || is_host_enabled(musb))
 		wake_lock_init(&musb->musb_wakelock, WAKE_LOCK_SUSPEND,
 						"musb_autosuspend_wake_lock");
+	pm_runtime_put(musb->controller);
 
 	status = musb_init_debugfs(musb);
 	if (status < 0)
